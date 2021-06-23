0abbeb1c59a5084713965920613a6d7a98f93f9d
wuxudongxd <wuxudongxd@gmail.com>
2021-06-23 16:18:20 +0800

    feat: 新增登录

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import { ProjectListScreen } from "screens/project-list";
+import { LoginScreen } from "screens/login";
+// import { ProjectListScreen } from "screens/project-list";
 
 function App() {
   return (
     <div className="App">
-      <ProjectListScreen />
+      {/* <ProjectListScreen /> */}
+      <LoginScreen />
     </div>
   );
 }
